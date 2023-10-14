c8bec26b8da54a4419a0b9e8ba6b7473cb645091
Usman Virk <usmanvirk11@gmail.com>
2023-10-14 23:48:48 +0200

    Final Commit

--- deep.py
+++ deep.py
@@ -1,1 +1,20 @@
-# Hello
+x = input("What is the Answer to the Great Question of Life, the Universe, and Everything?  ")
+
+x = x.lower()
+
+# print (x)
+
+x = x.strip()
+
+# print (x)
+
+x = x.replace("-", " ")
+
+# print (x)
+
+if x == "42" or x == "forty two":
+
+        print("Yes")
+
+else:
+        print("No")
